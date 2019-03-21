116886e1831745131e8ec510b0db84dc40e66c61
Levladutz <37593074+Levladutz@users.noreply.github.com>
2019-03-21 18:22:27 +0200

    more
    HAHAHAHAHAHA

--- Info/Edgy Trees/main.cpp
+++ Info/Edgy Trees/main.cpp
@@ -1,9 +1,71 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define ull unsigned long long
+const ull MOD = 1e9 + 7;
+ull n, k;
+vector<ull>g[100005];
+ull viz[100005];
+ull x, y, c;
+ull sum, cate;
+ull fpw( ull base, ull exp )
+{
+    ull r = 1;
 
+    while( exp )
+        {
+            if( exp % 2 == 1 )
+                r = ( r * base ) % MOD;
+
+            base = ( base * base ) % MOD;
+            exp = exp / 2;
+        }
+
+    return r;
+}
+ull f( ull n )
+{
+    return fpw( n, k );
+}
+ull dfs( ull i )
+{
+    viz[i] = 1;
+    cate++;
+
+    for( auto it : g[i] )
+        if( !viz[it] )
+            dfs( it );
+
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    cin >> n >> k;
+    sum = f( n );
+    // cout << sum << ' ';
+
+    for( ull i = 1; i < n; i++ )
+        {
+            cin >> x >> y >> c;
+
+            if( c == 0 )
+                {
+                    g[x].push_back( y );
+                    g[y].push_back( x );
+                }
+        }
+
+    for( ull i = 1; i <= n; i++ )
+        if( !viz[i] )
+            {
+                cate = 0;
+                dfs( i );
+                // cout << i << ' ' << cate << '\n';
+                sum = ( sum - f( cate ) ) % MOD ;
+
+                if( sum < 0 )
+                    sum = ( sum + MOD ) % MOD;
+
+            }
+
+    cout << sum;
     return 0;
 }
