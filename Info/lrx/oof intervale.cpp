85c6accd805b275aee4443ed8634ee80d15ad009
Levladutz <37593074+Levladutz@users.noreply.github.com>
2019-04-21 20:23:16 +0300

    woop

--- Info/lrx/oof intervale.cpp
+++ Info/lrx/oof intervale.cpp
@@ -4,7 +4,7 @@ using namespace std;
 ifstream fin ( "lrx.in" );
 ofstream fout ( "lrx.out" );
 int n, ans, nri;
-int a[1000000], b[1000000], dif[1000000];
+int a[10000], b[10000], dif[10000];
 struct diferenta
 {
     int v, i;
@@ -15,20 +15,12 @@ struct diferenta
 
         return a.i > i;
     }
-} v[1000000];
+} v[10000];
 struct interval
 {
     int l, r;
-
 };
-bool operator<( const interval &a, const interval &b )
-{
-    if( a.l != b.l )
-        return a.l > b.l;
-
-    return a.r > b.r;
-}
-set<interval>m;
+vector<interval>m;
 int ok( interval x )
 {
     return x.l <= x.r;
@@ -54,38 +46,41 @@ int main()
     interval x;
     x.l = 1;
     x.r = n;
-    m.insert( x );
+    m.push_back( x );
 
     for ( int i = 1; i <= n; i++ )
         {
             int nri = m.size();
             ///aduni la raspuns cate intervale ai pana acum inmultite cu inaltimea de la cel anterior la cel actial
             ans += nri * ( v[i].v - v[i - 1].v );
+            int j = i;
 
             ///scot elementele j egale cu i din intervale
             do
                 {
-                    for( auto it : m )
+                    int nri = m.size();
+
+                    for( int i = 0; i < nri; i++ )
                         {
-                            if( it.l <= v[i].i && it.r >= v[i].i )
+                            if( m[i].l <= v[i].i && m[i].r >= v[i].i )
                                 {
                                     interval s, d;
                                     //intervalul din stanga
-                                    s.l = it.l;
+                                    s.l = m[i].l;
                                     s.r = v[i].i - 1;
                                     //intervalul din dreapta
                                     d.l = v[i].i + 1;
-                                    d.r = it.r;
+                                    d.r = m[i].r;
                                     //sterg intervalul mare
-                                    m.erase( m.find( it ) );
+                                    m.erase( m.begin() + i );
 
                                     //daca s valida
                                     if( ok( s ) )
-                                        m.insert( s );
+                                        m.push_back( s );
 
                                     //daca d valida
                                     if( ok( d ) )
-                                        m.insert( d );
+                                        m.push_back( d );
                                 }
                         }
 
