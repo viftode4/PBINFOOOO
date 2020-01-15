ea6612ab7d1535d28f11a7b346082bc04da1ad7e
Levladutz <37593074+Levladutz@users.noreply.github.com>
2020-01-15 22:48:56 +0200

    pebe

--- Info/stramosi/main.cpp
+++ Info/stramosi/main.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
-
+#include <fstream>
 using namespace std;
-
+ifstream fin( "stramosi.in" );
+ofstream fout( "stramosi.out" );
+int n, m, nod, ord, s[250010][20];
 int main()
 {
-    cout << "Hello world!" << endl;
+    fin >> n >> m;
+
+    for( int i = 1; i <= n; ++i )
+        fin >> s[i][0] ;
+
+    for( int k = 1; k <= 16; ++k )
+        for( int i = 1; i <= n; i++ )
+            s[i][k] = s[s[i][k - 1]][k - 1];
+
+    for( int i = 1; i <= m; i++ )
+        {
+            fin >> nod >> ord;
+
+            for( int cat = 0; cat <= 16; ++cat )
+                if( ord & ( 1 << cat ) )
+                    nod = s[nod][cat];
+
+            fout << nod << '\n';
+        }
+
     return 0;
 }
